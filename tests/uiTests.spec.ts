eb45da6bfbf4e2c99f51a46fb04819cae9f66a97
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2025-10-16 21:04:32 +0300

    User facing locators

--- tests/uiTests.spec.ts
+++ tests/uiTests.spec.ts
@@ -47,3 +47,14 @@ test('Locator syntax rules', async ({ page }) => {
     // // by text with Tag Name
     // await page.locator('nb-card :text("Using the Grid")').click(); // can be same for multiple elements but more specific   
 });
+
+test('User facing locators', async ({ page }) => {
+    await page.getByRole('textbox', { name: 'Email' }).first().click();
+    await page.getByRole('button', {name: 'Sign in'}).first().click();
+
+    await page.getByLabel('Email').first().click();
+    await page.getByPlaceholder('Jane Doe').click();
+    await page.getByText('Using the Grid').click();
+    await page.getByTitle('IoT Dashboard').click();
+    await page.getByTestId('SignIn').click();
+});
