2f9618b30ef1776941c7094686fd381d2f212ee4
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2025-10-21 00:20:17 +0300

    Add sliders tests

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -219,3 +219,28 @@ test('datepicker', async ({ page }) => {
 
     await expect(datepickerInputField).toHaveValue(expectedDateFormatted);
 });
+
+test('sliders', async ({ page }) => {
+    // // Update attribute
+    // const tempGauge = page.locator('[tabtitle="Temperature"] ngx-temperature-dragger circle');
+    // await tempGauge.evaluate(element => {
+    //     element.setAttribute('cx', '232.6309883354377');
+    //     element.setAttribute('cy', '232.6309883354377');
+    // });
+    // await tempGauge.click();
+
+    // Mouse movement
+    const tempBox = page.locator('[tabtitle="Temperature"] ngx-temperature-dragger');
+    await tempBox.scrollIntoViewIfNeeded();
+
+    const box = await tempBox.boundingBox();
+    const x = box.x + box.width / 2;
+    const y = box.y + box.height / 2;
+    await page.mouse.move(x, y);
+    await page.mouse.down();
+    await page.mouse.move(x + 100, y);
+    await page.mouse.move(x + 100, y + 100);
+    await page.mouse.up();
+
+    await expect(tempBox).toContainText('30');
+});
