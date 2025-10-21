f5d3aa6bd4e2511cb6522ea3167e073286213bb2
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2025-10-21 22:17:37 +0300

    Add NavigationPage class. Use Page Objects to navigate to Form Layouts page

---
+++ tests/usePageObjects.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+import { NavigationPage } from '../page-objects/navigationPage';
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:4200/');
+});
+
+test('navigate to Form Layouts page', async ({ page }) => {
+    const navigateTo = new NavigationPage(page);
+    await navigateTo.formLayoutsPage();
+    await expect(page).toHaveURL('http://localhost:4200/pages/forms/layouts');
+});
