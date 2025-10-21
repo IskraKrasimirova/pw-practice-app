e8cd4a129719ffec79735e4cfa38430d6db12b0c
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2025-10-21 21:06:16 +0300

    Drag and Drop with iFrame

---
+++ tests/dragAndDropWithiFrame.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test('Drag and Drop with iFrame', async ({ page }) => {
+    await page.goto('https://www.globalsqa.com/demo-site/draganddrop/');
+
+    const frame = page.frameLocator('[rel-title="Photo Manager"] iframe');
+    await frame.locator('li', { hasText: 'High Tatras 2' }).dragTo(frame.locator('#trash'));
+    
+    await expect(frame.locator('#trash li')).toContainText('High Tatras 2');
+    expect.soft(await frame.locator('ul #gallery li', { hasText: 'High Tatras 2' }).count()).toBe(0);
+
+    // more precise control over drag and drop using mouse events
+    await frame.locator('li', { hasText: 'High Tatras 3' }).hover();
+    await page.mouse.down();
+    await frame.locator('#trash').hover();
+    await page.mouse.up();
+
+    await expect(frame.locator('#trash li h5')).toHaveText(['High Tatras 2', 'High Tatras 3']);
+});
