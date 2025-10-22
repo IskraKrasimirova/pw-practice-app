c7d92fd396a84f801b1daed33b91593aa5ce5a47
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2025-10-22 20:34:38 +0300

    Add PageManager class. Refactoring

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,35 +1,31 @@
 import { test, expect } from '@playwright/test';
-import { NavigationPage } from '../page-objects/navigationPage';
-import { FormLayoutsPage } from '../page-objects/formLayoutsPage';
-import { DatepickerPage } from '../page-objects/datepickerPage';
+import { PageManager } from '../page-objects/pageManager';
 
 test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4200/');
 });
 
 test('navigate to Form Layouts page', async ({ page }) => {
-    const navigateTo = new NavigationPage(page);
-    await navigateTo.formLayoutsPage();
+    const pm = new PageManager(page);
+    await pm.navigateTo().formLayoutsPage();
     await expect(page).toHaveURL('http://localhost:4200/pages/forms/layouts');
 
-    await navigateTo.datepickerPage();
+    await pm.navigateTo().datepickerPage();
     await expect(page).toHaveURL('http://localhost:4200/pages/forms/datepicker');
 
-    await navigateTo.toasterPage();
-    await navigateTo.smartTablePage();
-    await navigateTo.tooltipPage();
+    await pm.navigateTo().toasterPage();
+    await pm.navigateTo().smartTablePage();
+    await pm.navigateTo().tooltipPage();
 });
 
 test('parametrized methods', async ({ page }) => {
-    const navigateTo = new NavigationPage(page);
-    const onFormLayoutsPage = new FormLayoutsPage(page);
-    const onDatePickerPage = new DatepickerPage(page);
+    const pm = new PageManager(page);
 
-    await navigateTo.formLayoutsPage();
-    await onFormLayoutsPage.submitUsingTheGridFormWithCredentialsAndOption('user@user.com', 'Secret123', 'Option 1');
-    await onFormLayoutsPage.submitInlineFormWithNameEmailAndCheckbox('John Smith', 'user2@user.com', true);
+    await pm.navigateTo().formLayoutsPage();
+    await pm.onFormLayoutsPage().submitUsingTheGridFormWithCredentialsAndOption('user@user.com', 'Secret123', 'Option 1');
+    await pm.onFormLayoutsPage().submitInlineFormWithNameEmailAndCheckbox('John Smith', 'user2@user.com', true);
 
-    await navigateTo.datepickerPage();
-    await onDatePickerPage.selectCommonDatePickerDateFromToday(10);
-    await onDatePickerPage.selectDatePickerWithRangeFromToday(6, 15);
+    await pm.navigateTo().datepickerPage();
+    await pm.onDatePickerPage().selectCommonDatePickerDateFromToday(10);
+    await pm.onDatePickerPage().selectDatePickerWithRangeFromToday(6, 15);
 });
