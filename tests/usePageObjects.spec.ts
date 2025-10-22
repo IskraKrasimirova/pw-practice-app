883631757103863cffa4231a41dddb4d082c19d0
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2025-10-22 17:54:28 +0300

    Add FormLayoutsPage class with parametrized methods

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { NavigationPage } from '../page-objects/navigationPage';
+import { FormLayoutsPage } from '../page-objects/formLayoutsPage';
 
 test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4200/');
@@ -16,4 +17,12 @@ test('navigate to Form Layouts page', async ({ page }) => {
     await navigateTo.toasterPage();
     await navigateTo.smartTablePage();
     await navigateTo.tooltipPage();
+});
+
+test('parametrized methoda', async ({ page }) => {
+    const navigateTo = new NavigationPage(page);
+    const onFormLayoutsPage = new FormLayoutsPage(page);
+    await navigateTo.formLayoutsPage();
+    await onFormLayoutsPage.submitUsingTheGridFormWithCredentialsAndOption('user@user.com', 'Secret123', 'Option 1');
+    await onFormLayoutsPage.submitInlineFormWithNameEmailAndCheckbox('John Smith', 'user2@user.com', true);
 });
